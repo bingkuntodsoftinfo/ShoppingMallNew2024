cdb230df8eae2031c4bfba6b3f5f3e629d5744fb
bingkunotd <thawatchai@abc.com>
2024-02-08 22:09:56 +0700

    UpdateEP7

---
+++ lib/utility/my_constant.dart
@@ -0,0 +1,54 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class MyConstant {
+  
+// General
+static String appName='Shopping Mall';
+
+// Route
+static String routeAuthen='/authen';
+static String routeCreateAccount='/createAccount';
+static String routeBuyerService='/buyerService';
+static String routeSalerService='/salerService';
+static String routeRiderService='/riderService';
+
+// Image
+static String image1= 'images/image1.png';
+static String image2= 'images/image2.png';
+static String image3= 'images/image3.png';
+static String image4= 'images/image4.png';
+
+// Color 
+static Color primay = Color(0xff6750a4);
+static Color dark = Color(0xff22005d);
+static Color light  = Color(0xffe9ddff);
+
+// Style 
+
+TextStyle h1Style()=>TextStyle(
+  fontSize: 24,
+  color: dark,
+  fontWeight: FontWeight.bold,
+);
+
+TextStyle h2Style()=>TextStyle(
+  fontSize: 18,
+  color: dark,
+  fontWeight: FontWeight.bold,
+);
+
+
+TextStyle h3Style()=>TextStyle(
+  fontSize: 14,
+  color: dark,
+  fontWeight: FontWeight.bold,
+);
+
+
+}
+
+
