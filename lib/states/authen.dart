8ea5bd0d8a29057298599b6fd008e78d2b06bf8b
bingkunotd <thawatchai@abc.com>
2024-02-09 09:01:20 +0700

    Update

--- lib/states/authen.dart
+++ lib/states/authen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, sized_box_for_whitespace
+
 import 'package:flutter/material.dart';
 import 'package:shoppingmallnew/utility/my_constant.dart';
 import 'package:shoppingmallnew/widgets/show_image.dart';
@@ -11,30 +13,132 @@ class Authen extends StatefulWidget {
 }
 
 class _AuthenState extends State<Authen> {
+  bool statusRedEye = true;
+
   @override
   Widget build(BuildContext context) {
     double size = MediaQuery.of(context).size.width;
     return Scaffold(
       body: SafeArea(
-        child: ListView(
-          children: [
-            buildImage(size),
-            buildAppName()
-          ],
+        child: GestureDetector(
+          onTap: () => FocusScope.of(context).requestFocus(FocusNode()),
+          behavior: HitTestBehavior.opaque,
+          child: ListView(
+            children: [
+              buildImage(size),
+              buildAppName(),
+              buildUser(size),
+              buildPassword(size),
+              buildLogin(size),
+            ],
+          ),
         ),
       ),
     );
   }
 
-  Row buildAppName() {
-    return Row(mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ShowTitle(
-                title: MyConstant.appName,
-                textStyle: MyConstant().h1Style(),
+  Row buildLogin(double size) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          margin: EdgeInsets.symmetric(vertical: 16),
+          width: size * 0.6,
+          child: ElevatedButton(
+            style: MyConstant().myButtonStyle(),
+            onPressed: () {},
+            child: Text('Login'),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Row buildUser(double size) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          margin: const EdgeInsets.only(top: 16),
+          width: size * 0.6,
+          child: TextFormField(
+            decoration: InputDecoration(
+              labelStyle: MyConstant().h3Style(),
+              labelText: 'User :',
+              prefixIcon: Icon(
+                Icons.account_circle_outlined,
+                color: MyConstant.dark,
               ),
-            ],
-          );
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: MyConstant.dark),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: MyConstant.light),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Row buildPassword(double size) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          margin: EdgeInsets.only(top: 16),
+          width: size * 0.6,
+          child: TextFormField(
+            obscureText: statusRedEye,
+            decoration: InputDecoration(
+              suffixIcon: IconButton(
+                onPressed: () {
+                  setState(() {
+                    statusRedEye = !statusRedEye;
+                  });
+                },
+                icon: statusRedEye
+                    ? Icon(
+                        Icons.remove_red_eye,
+                        color: MyConstant.dark,
+                      )
+                    : Icon(
+                        Icons.remove_red_eye_outlined,
+                        color: MyConstant.dark,
+                      ),
+              ),
+              labelStyle: MyConstant().h3Style(),
+              labelText: 'Password :',
+              prefixIcon: Icon(
+                Icons.lock_outline,
+                color: MyConstant.dark,
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: MyConstant.dark),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: MyConstant.light),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Row buildAppName() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ShowTitle(
+          title: MyConstant.appName,
+          textStyle: MyConstant().h1Style(),
+        ),
+      ],
+    );
   }
 
   Row buildImage(double size) {
--- lib/utility/my_constant.dart
+++ lib/utility/my_constant.dart
@@ -5,50 +5,51 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 
 class MyConstant {
-  
 // General
-static String appName='Shopping Mall';
+  static String appName = 'Shopping Mall';
 
 // Route
-static String routeAuthen='/authen';
-static String routeCreateAccount='/createAccount';
-static String routeBuyerService='/buyerService';
-static String routeSalerService='/salerService';
-static String routeRiderService='/riderService';
+  static String routeAuthen = '/authen';
+  static String routeCreateAccount = '/createAccount';
+  static String routeBuyerService = '/buyerService';
+  static String routeSalerService = '/salerService';
+  static String routeRiderService = '/riderService';
 
 // Image
-static String image1= 'images/image1.png';
-static String image2= 'images/image2.png';
-static String image3= 'images/image3.png';
-static String image4= 'images/image4.png';
+  static String image1 = 'images/image1.png';
+  static String image2 = 'images/image2.png';
+  static String image3 = 'images/image3.png';
+  static String image4 = 'images/image4.png';
 
-// Color 
-static Color primay = Color(0xff6750a4);
-static Color dark = Color(0xff22005d);
-static Color light  = Color(0xffe9ddff);
+// Color
+  static Color primay = Color(0xff6750a4);
+  static Color dark = Color(0xff22005d);
+  static Color light = Color(0xffe9ddff);
 
-// Style 
+// Style
 
-TextStyle h1Style()=>TextStyle(
-  fontSize: 24,
-  color: dark,
-  fontWeight: FontWeight.bold,
-);
+  TextStyle h1Style() => TextStyle(
+        fontSize: 24,
+        color: dark,
+        fontWeight: FontWeight.bold,
+      );
 
-TextStyle h2Style()=>TextStyle(
-  fontSize: 18,
-  color: dark,
-  fontWeight: FontWeight.bold,
-);
+  TextStyle h2Style() => TextStyle(
+        fontSize: 18,
+        color: dark,
+        fontWeight: FontWeight.bold,
+      );
 
+  TextStyle h3Style() => TextStyle(
+        fontSize: 14,
+        color: dark,
+        fontWeight: FontWeight.bold,
+      );
 
-TextStyle h3Style()=>TextStyle(
-  fontSize: 14,
-  color: dark,
-  fontWeight: FontWeight.bold,
-);
-
-
+  ButtonStyle myButtonStyle() => ElevatedButton.styleFrom(
+        backgroundColor: MyConstant.light,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+      );
 }
-
-
